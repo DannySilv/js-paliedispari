6b397755176dc7e80e524f2df7d499049f626de0
DannySilv <danielesilvestre@outlook.it>
2022-04-21 15:42:14 +0200

    Completed OddOrEven w functions

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -4,34 +4,54 @@
 //4- Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //5- Dichiariamo chi ha vinto.
 
-// 1
-const oddOrEven = prompt("Vuoi essere pari o dispari?");
+//ODD OR EVEN AND NUMBER CHOICE
+let oddOrEven = prompt("Vuoi essere pari o dispari?");
+while (oddOrEven !== "pari" && oddOrEven !== "dispari") {
+    alert("Puoi scegliere solo tra pari e dispari! Riprova!");
+    oddOrEven = prompt("Vuoi essere pari o dispari?");  
+}
+console.log(oddOrEven);
 let userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
-
 while (userNumber > 5 || userNumber < 1) {
     alert("Il numero che hai scelto non va bene, scegline un altro!");
     userNumber = prompt("Scegli un numero da 1 a 5");  
 }
 console.log(userNumber);
 
-// 2
-const pcNumber = Math.floor(Math.random() * 5 + 1);
-console.log(pcNumber);
-
-// 3
-let sum = userNumber + pcNumber;
+// SUM
+let sum = userNumber + randomNumbGen();
 console.log(sum);
 
-// 4
-if (sum % 2 == 0) {
-    sum = "pari";
-} else {
-    sum = "dispari";
-}
-
-// 5
-if (userNumber == sum) {
+// COMPARE SUM TO ODDOREVEN TO CHECK WHO WINS
+if (oddOrEven == isSumOddOrEven(sum)) {
     alert("Complimenti! Hai vinto!");
 } else {
     alert("Mi dispiace, hai perso!");
+}
+
+// FUNCTIONS
+
+/**
+ * Description -> This function generates a random number between 1 and 5
+ * @returns {Number} -> Returns the number generated
+ */
+function randomNumbGen() {
+    const pcNumber = Math.floor(Math.random() * 5 + 1);
+    console.log(pcNumber);
+    return pcNumber;
+}
+
+/**
+ * Description -> This function check the sum of two numbers and declares if it is odd or even
+ * @param {Integer} sum -> The sum obtained by adding together the user number and the pc number
+ * @returns {Number} -> Returns a string saying if the sum is odd or even
+ */
+function isSumOddOrEven(sum) {
+    if (sum % 2 == 0) {
+        sum = "pari";
+    } else {
+        sum = "dispari";
+    }
+    console.log(sum);
+    return sum;
 }
