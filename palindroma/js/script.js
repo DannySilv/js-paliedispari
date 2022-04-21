6330f123e8ca8779d29e68f8d1f5619de20cbe71
DannySilv <danielesilvestre@outlook.it>
2022-04-21 13:36:36 +0200

    Completed Palindrome Exercise w Function

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -3,14 +3,8 @@
 
 const userWord = prompt("Inserisci una parola");
 console.log(userWord);
-const userWordSplit = userWord.split("");
-console.log(userWordSplit);
-const userWordReverse = userWordSplit.reverse();
-console.log(userWordReverse);
-const userWordJoin = userWordReverse.join("");
-console.log(userWordJoin);
 
-if (userWord == userWordJoin) {
+if (userWord == isPalindrome(userWord)) {
     alert("La parola è palindroma!")
 } else {
     alert("La parola non è palindroma!")
@@ -18,4 +12,17 @@ if (userWord == userWordJoin) {
 
 // FUNCTIONS
 
+/**
+ * Description -> This function split the string choosen by the user in various arrays, changes the order of the letters by switching the new-created array elements position, rejoin the elements in a single string and returns the obtained new string.
+ * @param {String} userWord -> The word choosen by the user
+ * @returns {String} -> The word with letters order switched
+ */
+
+function isPalindrome(userWord) {
+    const userWordSplit = userWord.split("");
+    const userWordReverse = userWordSplit.reverse();
+    const userWordJoin = userWordReverse.join("");
+    console.log(userWordJoin)
+    return userWordJoin;
+}
 
